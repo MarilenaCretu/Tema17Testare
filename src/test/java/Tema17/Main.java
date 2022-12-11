707a00c117ff54755ab4aaf8ccfbe5b587324c6e
MarilenaCretu <marilenac2001@gmail.com>
2022-12-11 15:22:24 +0200

    Commit

--- src/test/java/Tema17/Main.java
+++ src/test/java/Tema17/Main.java
@@ -1,2 +1,9 @@
-package Tema17;public class Main {
+package Tema17;
+
+public class Main {
+    public static void main(String[] args) {
+        Pozele pozele=new Pozele();
+       // pozele.addToCart();
+       // pozele.addPozition();
+    }
 }
--- src/test/java/Tema17/Pozele.java
+++ src/test/java/Tema17/Pozele.java
@@ -1,2 +1,41 @@
-package Tema17;public class Pozele {
+package Tema17;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Pozele {
+    private WebDriver driver;
+
+    @Before
+    public void getWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+    }
+
+    @Test
+    //poza 1
+    public void addToCart() {
+
+        driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-5.parent > a")).click();
+        driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li > div > h2 > a")).click();
+        driver.findElement(By.cssSelector("body > div.wrapper"));
+        driver.findElement(By.cssSelector("#swatch27 > span.swatch-label > img")).click();
+        driver.findElement(By.cssSelector("#swatch81 > span.swatch-label")).click();
+        driver.findElement(By.cssSelector("#product_addtocart_form > div.product-shop > div.product-options-bottom > div.add-to-cart > div.add-to-cart-buttons > button > span > span")).click();
+        driver.findElement(By.cssSelector("#search_mini_form > div.input-box > button")).click();
+
+    }
+
+
+    @After
+    public void close() {
+        driver.close();
+    }
 }
